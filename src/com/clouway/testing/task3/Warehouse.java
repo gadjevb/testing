844d3061b9fdf45bb6ad5450fdcc10d915f5edd4
Borislav Gadjev <gadjevb@gmail.com>
2016-06-22 10:46:38 +0300

    Final commit to task3, Warehouse is ready for review

--- src/com/clouway/testing/task3/Warehouse.java
+++ src/com/clouway/testing/task3/Warehouse.java
@@ -6,6 +6,12 @@ public class Warehouse {
     private int spaceForMerchandise = 2;
     ArrayList<Merchandise> stock = new ArrayList<>();
 
+    /*
+    addMerchandise() methods adds the given merchandise to the stock list after a check for space in the Warehouse
+    If spaceForMerchandise is greater than 0, the merchandise will be saved and spaceForMerchandise will be decremented,
+    otherwise WarehouseIsFullException will be thrown.
+     */
+
     public void addMerchandise(Merchandise object) throws WarehouseIsFullException {
         if(spaceForMerchandise > 0){
             spaceForMerchandise--;
@@ -15,10 +21,22 @@ public class Warehouse {
         }
     }
 
+    /**
+     getMerchandiseAtPosition() is NOT required for the task,
+     it's a helping method for the tests
+     */
+
     public Merchandise getMerchandiseAtPosition(int i) {
         Merchandise temp = stock.get(i);
         return temp;
     }
+
+    /*
+    sellMerchandise() method sells merchandise by the given characteristics and budget of the buyer,
+    if a merchandise that fulfills the characteristics is found a True will be returned and the merchandise will be removed from the warehouse
+    and spaceForMerchandise will be incremented, if there is no such merchandise False will be returned this means that there is no such merchandise that
+    fulfills the buyer requirements. If the Warehouse is empty the method will throw WarehouseIsEmptyException.
+     */
 
     public boolean sellMerchandise(String objectType, String objectModel, int objectPrice) throws WarehouseIsEmptyException {
         if(stock.isEmpty()){
@@ -36,5 +54,12 @@ public class Warehouse {
         return false;
     }
 
+    /*
+    sortByPrice() method, I defined a custom comparator because it makes life easier 
+     */
+
+    public void sortByPrice() {
+        Collections.sort(stock,new PriceComparator());
+    }
 
 }
--- src/com/clouway/testing/test/task3/WarehouseTDD.java
+++ src/com/clouway/testing/test/task3/WarehouseTDD.java
@@ -6,6 +6,11 @@ import org.junit.*;
 
 public class WarehouseTDD {
 
+    /**
+    The first two test check is the Warehouse and Merchandise objects
+     are created.
+     */
+
     @Test
     public void createWarehouseObject(){
         Warehouse warehouse = new Warehouse();
@@ -18,8 +23,13 @@ public class WarehouseTDD {
         assertNotNull(temp);
     }
 
+    /**
+    The next two test check the addMerchandise() method for the Warehouse class
+     and the WarehouseIsFullException
+     */
+
     @Test
-    public void addMerchendiseToWarehouse() {
+    public void addMerchandiseToWarehouse() {
         Warehouse warehouse = new Warehouse();
         Merchandise merch1 = new Merchandise("Fridge","Bosch",800);
         Merchandise temp = new Merchandise("Fridge","Bosch",800);
@@ -45,6 +55,11 @@ public class WarehouseTDD {
         warehouse.addMerchandise(merch3);
     }
 
+    /**
+     The next three test check the sellMerchandise() method when the searched product is found,
+     when the searched product is NOT found and the WarehouseIsEmptyException
+     */
+
     @Test
     public void sellMerchandise(){
         boolean flag = false;
@@ -65,14 +80,66 @@ public class WarehouseTDD {
         assertTrue(flag);
     }
 
-    @Test(expected = WarehouseIsEmptyException.class)
+    @Test
+    public void wantedMerchandiseNotFoundInWarehouse(){
+        boolean flag = true;
+        Warehouse warehouse = new Warehouse();
+        Merchandise merch1 = new Merchandise("Fridge","Bosch",800);
+        Merchandise merch2 = new Merchandise("Televisor","Neo",430);
+        try {
+            warehouse.addMerchandise(merch1);
+            warehouse.addMerchandise(merch2);
+        } catch (WarehouseIsFullException e) {
+            e.printStackTrace();
+        }
+        try {
+            flag = warehouse.sellMerchandise("Microwave","Bosch",80);
+        } catch (WarehouseIsEmptyException e) {
+            e.printStackTrace();
+        }
+        assertFalse(flag);
+    }
+
+    @Test
     public void sellMerchandiseFromEmptyWarehouse(){
         Warehouse warehouse = new Warehouse();
         try {
             warehouse.sellMerchandise("Televisor","Sony",560);
         } catch (WarehouseIsEmptyException e) {
-            e.printStackTrace();
+            e.getMessage();
         }
     }
 
+    /**
+    The last two test check for the sortByPriceMethod() in Warehouse
+     when there is merchandise and when there is NO merchandise
+     */
+
+    @Test
+    public void sortWarehouseMerchandiseByPrice(){
+        Warehouse warehouse = new Warehouse();
+        Warehouse sortedWarehouse = new Warehouse();
+        Merchandise merch1 = new Merchandise("Fridge","Bosch",800);
+        Merchandise merch2 = new Merchandise("Televisor","Neo",430);
+        try {
+            warehouse.addMerchandise(merch1);
+            warehouse.addMerchandise(merch2);
+            sortedWarehouse.addMerchandise(merch2);
+            sortedWarehouse.addMerchandise(merch1);
+        } catch (WarehouseIsFullException e) {
+            e.printStackTrace();
+        }
+        warehouse.sortByPrice();
+        Merchandise temp = warehouse.getMerchandiseAtPosition(0);
+        Merchandise sortedTemp = sortedWarehouse.getMerchandiseAtPosition(0);
+        assertEquals(temp.getType(), sortedTemp.getType());
+        assertEquals(temp.getModel(), sortedTemp.getModel());
+        assertEquals(temp.getPrice(), sortedTemp.getPrice());
+    }
+
+    @Test
+    public void sortEmptyWarehouseMerchandiseByPrice(){
+        Warehouse warehouse = new Warehouse();
+        warehouse.sortByPrice();
+    }
 }
