7a10c81539113b295e36fcfc8c1a545c37d42b25
Borislav Gadjev <gadjevb@gmail.com>
2016-06-28 11:59:51 +0300

    Task4 - EndpointFilter is ready!

--- src/com/clouway/testing/test/task4/EndpointFilterTest.java
+++ src/com/clouway/testing/test/task4/EndpointFilterTest.java
@@ -12,7 +12,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class EndpointTest {
+public class EndpointFilterTest {
     @Rule public JUnitRuleMockery context = new JUnitRuleMockery();
     Endpoint endpoint = context.mock(Endpoint.class);
 
---
+++ src/com/clouway/testing/test/task4/StartsWithKeyWordTest.java
@@ -0,0 +1,7 @@
+package com.clouway.testing.test.task4;
+
+/**
+ * Created by clouway on 28.06.16.
+ */
+public class StartsWithKeyWordTest {
+}
